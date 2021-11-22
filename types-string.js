acbe60ba2195e39723427576ea3aa69911bd07dc
Aishwarya Goyal <aishwaryag@crowdstaffing.com>
2021-11-22 13:08:26 +0530

    First assignment, string type

---
+++ types-string.js
@@ -0,0 +1,11 @@
+/* First Assignment */
+
+const name = "Aishwarya";
+const hello =
+  "Hello. My name is " +
+  name +
+  ". I am trying concatenation using + operator and interpolation also.";
+const helloBackTicks = `Hello. My name is ${name}. This is how we do it with back ticks. A lot easier no?`;
+
+console.log(hello);
+console.log(helloBackTicks);
